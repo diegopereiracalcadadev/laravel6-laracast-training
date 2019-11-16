45caebc797f6526ae4391c0a3cdb6466b19d08d9
diegopereiracalcadadev <diegopereiracalcada@gmail.com>
2019-11-16 02:55:01 -0300

    Criado gerenciamento de usuários

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,28 @@
+<table>
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>name</th>
+            <th>login</th>
+            <th>cpf</th>
+            <th>email</th>
+            <th>password</th>
+        </tr>
+    </thead>
+    <tbody>
+        
+        @foreach($users as $user)
+        <tr>
+            <td>{{$user->id}}</td>
+            <td>{{$user->name}}</td>
+            <td>{{$user->login}}</td>
+            <td>{{$user->cpf}}</td>
+            <td>{{$user->email}}</td>
+            <td>{{$user->password}}</td>
+        </tr>
+
+     
+        @endforeach
+
+    </tbody>
+</table>
